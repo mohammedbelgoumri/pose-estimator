e966af7cd733d73e9ad3d43aa75067631ab22af6
mohammedbelgoumri <mohammedbelgoumri@gmail.com>
2022-08-18 13:20:18 +0100

    python.solutions

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import numpy as np
 import cv2
-import mediapipe as mp
+from mediapipe.python.solutions import pose, drawing_utils, drawing_styles
+from matplotlib import pyplot as plt
 
 def get_angle(a, b, c):
     a = np.array(a)
@@ -24,8 +25,8 @@ def count_curls():
     cap = cv2.VideoCapture(0) if is_webcam else cv2.VideoCapture("assets/video.mp4")
     cap.set(cv2.CAP_PROP_FRAME_WIDTH, WIDTH)
     cap.set(cv2.CAP_PROP_FRAME_HEIGHT, HEIGHT)
-    detector = mp.solutions.pose
-    drawer = mp.solutions.drawing_utils
+    detector = pose
+    drawer = drawing_utils
 
 
     counter = 0
@@ -35,7 +36,7 @@ def count_curls():
     with detector.Pose(
         min_detection_confidence=.5,
         min_tracking_confidence=.5
-    ) as pose:
+    ) as poser:
         while cap.isOpened():
             success, image = cap.read()
             if not success:
@@ -44,7 +45,7 @@ def count_curls():
             # annotated_image = image.copy()
             image.flags.writeable = False
             image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-            results = pose.process(image)
+            results = poser.process(image)
 
             image.flags.writeable = True
             image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
@@ -83,8 +84,8 @@ def count_curls():
             drawer.draw_landmarks(
                 image,
                 results.pose_landmarks,
-                mp.solutions.pose.POSE_CONNECTIONS,
-                landmark_drawing_spec=mp.solutions.drawing_styles.get_default_pose_landmarks_style()
+                pose.POSE_CONNECTIONS,
+                landmark_drawing_spec=drawing_styles.get_default_pose_landmarks_style()
             )
             im = cv2.resize(image, (WIDTH, HEIGHT))
             cv2.imshow("Face Detection", im)
@@ -99,3 +100,9 @@ def count_curls():
         for image in labeled:
             output.write(image)
         output.release()
+
+
+def draw_skeleton(landmarks, detector):
+    pass
+    
+
