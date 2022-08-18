48dd1bd9eaacf9ad8092171f187391f0040ecd7f
mohammedbelgoumri <mohammedbelgoumri@gmail.com>
2022-08-18 13:40:43 +0100

    get coords

--- utils.py
+++ utils.py
@@ -101,8 +101,34 @@ def count_curls():
             output.write(image)
         output.release()
 
+def get_coords(landmarks):
+    # get coordinates:
+    names = {
+        "NOSE": 0,
+        "LEFT_SHOULDER": 11,
+        "RIGHT_SHOULDER": 12,
+        "LEFT_ELBOW": 13,
+        "RIGHT_ELBOW": 14,
+        "LEFT_WRIST": 15,
+        "RIGHT_WRIST": 16,
+        "LEFT_PINKY": 17,
+        "RIGHT_PINKY": 18,
+        "LEFT_INDEX": 19,
+        "RIGHT_INDEX": 20,
+        "LEFT_THUMB": 21,
+        "RIGHT_THUMB": 22,
+        "LEFT_HIP": 23,
+        "RIGHT_HIP": 24,
+        "LEFT_KNEE": 25,
+        "RIGHT_KNEE": 26,
+        "LEFT_ANKLE": 27,
+        "RIGHT_ANKLE": 28,
+        "LEFT_HEEL": 29,
+        "RIGHT_HEEL": 30,
+        "LEFT_FOOT_INDEX": 31,
+        "RIGHT_FOOT_INDEX": 32
+    }
 
-def draw_skeleton(landmarks, detector):
-    pass
+    return {landmark: (landmarks[landmark].x, landmarks[landmark].y) for landmark in names}
     
 
